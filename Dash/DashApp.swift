a829618f38c02ec98d8feda04baf5ade8249ccea
beck <beck22j@gmail.com>
2026-07-05 07:40:57 +0900

    Initialize Dash iOS project

---
+++ Dash/DashApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct DashApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ DashFeature/Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "DashFeature",
+  products: [
+    .library(
+      name: "DashFeature",
+      targets: ["DashFeature"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "DashFeature",
+      path: "DashFeature"
+    ),
+    .testTarget(
+      name: "DashFeatureTests",
+      dependencies: ["DashFeature"],
+      path: "DashFeatureTests"
+    ),
+  ],
+  swiftLanguageModes: [.v6]
+)
